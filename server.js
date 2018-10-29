6b1ff16f75961a4aa03fb46ae586927c6605f322
Israel <israel.o@itthynk.co.za>
2018-10-29 14:20:49 +0200

    finished auth flow

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./service/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 5000;
 
+require('./routes/authRoutes')(app);
+
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT);
+
+
+mongodb://izzyfezzy:izzyfezzy1*@ds143953.mlab.com:43953/emailing-prod
